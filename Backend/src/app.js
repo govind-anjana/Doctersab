64142b24aeab808408495c978f469d2bf227953a
govind-anjana <govindanjana2004@gmail.com>
2025-11-21 16:59:06 +0530

    edit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import sendotpRoutes from '../src/routes/Sentotp.js'
+// import sendotpRoutes from '../src/routes/Sentotp.js'
 dotenv.config();
 const app=express();
 
--- Backend/src/routes/Sentotp.js
+++ Backend/src/routes/Sentotp.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import { sendOtp} from '../Controller/sendotpController.js';
-const router=express.Router();
+// import express from 'express';
+// import { sendOtp} from '../Controller/sendotpController.js';
+// const router=express.Router();
 
-router.post("/",sendOtp);
+// router.post("/",sendOtp);
 
-export default router
+// export default router
