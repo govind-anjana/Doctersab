5b8fd604b1990318940b05c02818fa949a763906
govind-anjana <govindanjana2004@gmail.com>
2025-12-01 12:40:20 +0530

    start to project in the docter sab

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -5,11 +5,10 @@ import connectDB from './config/db.js';
 dotenv.config();
 
 app.use(cors());
-// app.use(express.json())
 
 const PORT=process.env.PORT || 8080 ;
 connectDB();
 
 app.listen(PORT,()=>{
-    console.log(`Server Start : http://localhost:${PORT}`)
+    console.log(`npm Start : http://localhost:${PORT}`)
 })
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,6 @@ const app=express();
 
 app.use(express.json());
 
-app.use("/otp",sendotpRoutes)
+app.use("auth",)
 
 export default app;
